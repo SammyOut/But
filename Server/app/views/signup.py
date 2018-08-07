245e4ca690c902eb0c79113bfd505e6172356cb5
RISMME <python@istruly.sexy>
2018-08-07 16:10:32 +0900

    DONE: Signup

--- Server/app/views/signup.py
+++ Server/app/views/signup.py
@@ -2,8 +2,11 @@ from flasgger import swag_from
 from flask import request
 from flask_restful import Api
 
+from uuid import uuid4
+
 from app import signup_blueprint
 from app.docs.signup import *
+from app.models.user import UserModel
 from app.views import BaseResource
 
 api = Api(signup_blueprint)
@@ -14,4 +17,10 @@ api.prefix = '/signup'
 class Signup(BaseResource):
     @swag_from(SIGNUP_POST)
     def post(self):
-        pass
+        payload = request.json
+        uuid = uuid4()
+        payload['uuid'] = uuid
+
+        UserModel(**payload).save()
+
+        return self.unicode_safe_json_dumps({'user_id': uuid}, 201)
